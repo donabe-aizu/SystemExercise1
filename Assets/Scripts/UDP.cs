f550098346a492e09daa537ca8ad2afce0dc2872
donabe <donabe722@gmail.com>
2022-07-06 03:26:04 +0900

    add UDP

---
+++ Assets/Scripts/UDP.cs
@@ -0,0 +1,32 @@
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class UDP
+{
+    private UdpClient _client;
+        
+    public UDP(string ip, int port)
+    {
+        _client = new UdpClient();
+        _client.Connect(ip, port);
+    }
+
+    public async UniTaskVoid Send(string data, CancellationToken token = default)
+    {
+        byte[] message = Encoding.UTF8.GetBytes(data);
+        await _client.SendAsync(message, message.Length);
+        Debug.Log("Send");
+    }
+
+    public async UniTask<string> Receive(CancellationToken token = default)
+    {
+        var result = await _client.ReceiveAsync();
+        Debug.Log("Receive");
+        byte[] getByte = result.Buffer;
+        string data = Encoding.UTF8.GetString(getByte);
+        return data;
+    }
+}
---
+++ Assets/Scripts/UDPTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class UDPTest : MonoBehaviour
+{
+    [SerializeField]
+    private string _sendData = "test";
+    [SerializeField]
+    private string host = "192.168.1.8";
+    [SerializeField]
+    private int port = 9000;
+        
+    private UDP _udp;
+
+    private void Start()
+    {
+        _udp = new UDP(host,port);
+            
+        var token = this.GetCancellationTokenOnDestroy();
+        WaitReceive(token).Forget();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            _udp.Send(_sendData).Forget();
+        }
+    }
+        
+    async UniTaskVoid WaitReceive(CancellationToken token)
+    {
+        while (!token.IsCancellationRequested)
+        {
+            string data = await _udp.Receive(token);
+            Debug.Log(data);
+            await UniTask.Delay(TimeSpan.FromSeconds(3), cancellationToken: token);
+        }
+    }
+}
